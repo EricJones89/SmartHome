43d0365574c83ab76c9b93d32092eeb6b407b842
Eric Jones <Eric.Jones@asynchrony.com>
2014-11-23 21:08:01 -0600

    Now redirects to new programming view. #4

--- SmartHome/xbee_module/views.py
+++ SmartHome/xbee_module/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from xbee_module.models import xbee_module
 
+def index(request):
+    return HttpResponse("this is not programming")
+
 def program(request):
     context = {'serial_list':"here"}
-    return render(request, "xbee_module/index.html", context)
+    return HttpResponse("this is now programming")
